c2821f2d31f95a4edc72d761cd34c3d67878962e
frankie <fhuster@grameenfoundation.org>
2014-09-04 20:33:52 -0700

    basic auth
    Change-Id: I6f52a8a067ed5a1c2c8a2e7f57e225364e132087

--- sms/src/test/java/org/motechproject/sms/it/SendControllerIT.java
+++ sms/src/test/java/org/motechproject/sms/it/SendControllerIT.java
@@ -1,5 +1,6 @@
 package org.motechproject.sms.it;
 
+import org.apache.http.HttpStatus;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.entity.StringEntity;
@@ -7,18 +8,24 @@ import org.codehaus.jackson.map.ObjectMapper;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.motechproject.security.service.MotechUserService;
 import org.motechproject.sms.audit.SmsRecordsDataService;
 import org.motechproject.sms.service.OutgoingSms;
 import org.motechproject.testing.osgi.BasePaxIT;
 import org.motechproject.testing.osgi.TestContext;
 import org.motechproject.testing.osgi.container.MotechNativeTestContainerFactory;
+import org.motechproject.testing.osgi.http.SimpleHttpClient;
 import org.ops4j.pax.exam.ExamFactory;
 import org.ops4j.pax.exam.junit.PaxExam;
 import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
 import org.ops4j.pax.exam.spi.reactors.PerSuite;
 
 import javax.inject.Inject;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Locale;
+
+import static org.junit.Assert.assertTrue;
 
 /**
  * Verify SendController present & functional.
@@ -30,10 +37,22 @@ public class SendControllerIT extends BasePaxIT {
     @Inject
     private SmsRecordsDataService smsRecordsDataService;
 
+    @Inject
+    private MotechUserService motechUserService;
+
+    private final static String USERNAME = "username";
+    private final static String PASSWORD = "password";
+
     @Before
     public void waitForBeans() {
         // To prevent the annoying "BeanFactory not initialized or already closed" errors
         try { Thread.sleep(2000); } catch (InterruptedException e) {  }
+    }
+
+    @Before
+    public void createUser() {
+        motechUserService.register(USERNAME, PASSWORD, "user@email.com", "user.id", new ArrayList<String>(), Locale.US);
+        getLogger().info("Created user '{}'", USERNAME);
     }
 
     @Test
@@ -53,10 +72,8 @@ public class SendControllerIT extends BasePaxIT {
         httpPost.setEntity(new StringEntity(outgoingSmsJson));
 
         //TODO: Enable code below when we figure out a way around security
-        /*
         // We're specifying a nonexistent config so the controller should respond with a 404
-        assertTrue(SimpleHttpClient.execHttpRequest(httpPost, HttpStatus.SC_NOT_FOUND));
-        */
+        assertTrue(SimpleHttpClient.execHttpRequest(httpPost, HttpStatus.SC_NOT_FOUND, USERNAME, PASSWORD));
 
         //TODO: Also figure out how to create configs an then use them to pretend send using an SimpleHttpServer that
         //TODO: responds the way an SMS provider would.
